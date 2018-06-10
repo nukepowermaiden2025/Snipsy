15f4a6add30b5d8ca655424237a02dcd631ecccd
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-06-10 10:48:48 -0700

    Added meta tags to html

--- routes/todo/saveArticle.js
+++ routes/todo/saveArticle.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/save-article/:id', function(req, res, next) {
+  db.Article.findOneAndUpdate(
+    { _id: req.params.id },
+    { note: dbNote._id }, 
+    { new: true }//return the newest change
+);;
+
+  
   res.send('respond with a resource of saved article');
 });
 
 module.exports = router;
+db.Article.findOneAndUpdate(
+    { _id: req.params.id },
+    { note: dbNote._id }, 
+    { new: true }//return the newest change
+);
